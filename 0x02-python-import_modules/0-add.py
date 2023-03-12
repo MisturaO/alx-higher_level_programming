ef7e5d6fbae46e1ffb9be3c1bb30f27bb77c25cf
MisturaO <misturaojulari91@gmail.com>
2023-03-12 15:18:28 -0700

    prints the result of the addition 1 + 2 = 3

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Executing modules as scripts.Makes the file usable
-    as a script as well as an importable module.The code 
-    that parses the command line only runs if the module 
-    is executed as the “main” file
-    """
+    """The code that parses the command line only runs if the module 
+    is executed as the “main” file"""
     from add_0 import add
 
     a = 1
