07c64da22a75305400bc577d149a75307aa69a34
MisturaO <misturaojulari91@gmail.com>
2023-03-13 03:12:42 -0700

    imports the calculator functions from calculator_1.py fle

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     """the code that parses the command line only runs
-   if the module is executed as the “main” file"""
+    if the module is executed as the “main” file"""
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
