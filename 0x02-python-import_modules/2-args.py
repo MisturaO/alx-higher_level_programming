5b82555a8567b555415458efbcf973f0bb0d378e
MisturaO <misturaojulari91@gmail.com>
2023-03-13 06:32:08 -0700

    prints the number of and the list of its arguments.

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+    """prints the number of and the list of its arguments"""
+    lenght = len(argv)
+    lenght = lenght - 1
+
+    if lenght == 0:
+        print("{} arguments.".format(lenght))
+    elif lenght == 1:
+        print("{} argument:".format(lenght))
+    else:
+        print("{} arguments.".format(lenght))
+    for num in range(lenght):
+        print("{}: {}".format(num + 1, argv[num + 1]))
