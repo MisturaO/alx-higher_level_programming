d1237888835f19d474daffd370a49faaed369fec
MisturaO <misturaojulari91@gmail.com>
2023-03-16 11:33:15 -0700

    finds the biggest integer of a list.

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    """finds the biggest integer of a list."""
+    max_1 = my_list[0]
+    if len(my_list) == 0:
+        return None
+    for i in range(1, len(my_list)):
+        if my_list[i] > max_1:
+            max_1 = my_list[i]
+    return max_1
