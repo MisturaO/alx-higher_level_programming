0158378cf8e0ed13f4328f9d04e5ff7eb12a2977
MisturaO <misturaojulari91@gmail.com>
2023-03-15 17:35:02 -0700

    replaces an element in a list at a specific position

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    """replaces an element in a list at a specific position without modifying the original lis"""
+    """replaces an element in a list without modifying the original list"""
     if idx < 0:
         return my_list
     elif idx >= len(my_list):
