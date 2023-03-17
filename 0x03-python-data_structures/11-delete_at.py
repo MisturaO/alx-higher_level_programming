d93f976b8d5a7c8d27e978f5bb2baeb1caed6d4e
MisturaO <misturaojulari91@gmail.com>
2023-03-17 02:31:48 -0700

    deletes the item at a specific position in a list.

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,7 +4,7 @@ def delete_at(my_list=[], idx=0):
     """deletes the item at a specific position in a list."""
     if idx < 0:
         return my_list
-    elif my_list[idx] > (len(my_list) - 1):
+    if my_list[idx] > len(my_list):
         return my_list
     else:
         del my_list[idx]
