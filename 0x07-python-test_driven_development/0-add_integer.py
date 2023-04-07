ac244fe6a95817f303281a470b20cce150f5b1b8
MisturaO <misturaojulari91@gmail.com>
2023-04-07 10:47:29 -0700

    add 2 numbers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 """adds 2 integers."""
 
+
 def add_integer(a, b=98):
-    """
-    args are type casted to int or float, or first casted
+    """args are type casted to int or float, or first casted
     to int if they are float.
 
     Raises:
     TypeError(If either are non int or non float)
 
-    Returns an integer: the addition of a and b
-    """
-    if ((not isinstance(a, float) and not isinstance(a, int)):
+    Returns an integer: the addition of a and b"""
+    if((not isinstance(a, float) and not isinstance(a, int))):
         raise TypeError("a must be an integer")
-    if ((not isinstance(b, float) and not isinstance(b, int))):
+    if((not isinstance(b, float) and not isinstance(b, int))):
         raise TypeError("b must be an integer")
-    return (int(a) + int(b))
+    return int(a) + int(b)
