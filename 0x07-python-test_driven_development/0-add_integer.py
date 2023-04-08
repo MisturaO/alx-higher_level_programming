b1975d6b633ca08529390ddb9f464505449cf25c
MisturaO <misturaojulari91@gmail.com>
2023-04-08 02:01:07 -0700

    add integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,11 +5,16 @@
 def add_integer(a, b=98):
     """args are type casted to int or float, or first casted
     to int if they are float.
+    Args:
+        a: first param
+        b: second param
 
     Raises:
-    TypeError(If either are non int or non float)
+            TypeError(If either are non int or non float)
 
-    Returns an integer: the addition of a and b"""
+    Returns:
+            the addition of a and b
+    """
     if((not isinstance(a, float) and not isinstance(a, int))):
         raise TypeError("a must be an integer")
     if((not isinstance(b, float) and not isinstance(b, int))):
