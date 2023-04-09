850bfa5ab35ce504865aca2bdd41db701c173687
MisturaO <misturaojulari91@gmail.com>
2023-04-09 10:25:04 -0700

    returns string representation of the class

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""A Rectangle class that defines a rectangle"""
+
+
+class Rectangle():
+    """defines a rectangle"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get and set the width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get and set the height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Returns the rectangle area"""
+        self.a = self.height * self.width
+        return self.a
+
+    def perimeter(self):
+        """Returns the rectangle perimeter"""
+        if self.width == 0 or self.height == 0:
+            self.perim = 0
+        else:
+            self.perim = (self.height * 2) + (self.width * 2)
+        return self.perim
+
+    def __str__(self):
+        """prints string representation of the rectangle"""
+        rec_string = ""
+        if self.width == 0 or self.height == 0:
+            return("")
+        rec_string += "\n".join("#" * self.__width
+                                for i in range(self.__height))
+        return rec_string
+
+    def __repr__(self):
+        """Returns a string representation of the rectangle
+        so as to print the rectangle class itself to output:
+        Rectangle(arg1, arg2)"""
+        return ("Rectangle({:d}, {:d})".format(self.__width, self.__height))
