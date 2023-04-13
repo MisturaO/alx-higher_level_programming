77c8b1f30ff8781078cdc29258088a3699e4da86
MisturaO <misturaojulari91@gmail.com>
2023-04-13 13:48:43 -0700

    prints the list, but sorted (ascending sort)

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,7 +6,9 @@ a subclass of the list class"""
 class MyList(list):
     """prints the list, but sorted (ascending sort)"""
     def __init__(self):
+        """initializes the class instance"""
         super().__init__()
 
     def print_sorted(self):
+        """prints the list, but sorted (ascending sort)"""
         print(sorted(self))
