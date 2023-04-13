2ac1721f524f5aaede5240abb8d9d531de02cb57
MisturaO <misturaojulari91@gmail.com>
2023-04-13 13:20:08 -0700

    a class MyList that inherits from list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""class named 'MyList' that inherits from 'list'
+a subclass of the list class"""
+
+
+class MyList(list):
+    """prints the list, but sorted (ascending sort)"""
+    def __init__(self):
+        super().__init__()
+
+    def print_sorted(self):
+        print(sorted(self))
