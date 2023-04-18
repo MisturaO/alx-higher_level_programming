01c63e925e886ffa52cb21af7728b26a3fb284eb
MisturaO <misturaojulari91@gmail.com>
2023-04-18 07:13:47 -0700

    reads a text file (UTF8) and prints it to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,5 @@
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout"""
     with open(filename, mode='r', encoding="utf-8") as f:
-        print("{}".format(f.read(), end=''))
+        string = f.read()
+        print(string, end='')
