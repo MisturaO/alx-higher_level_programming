5c5e8f0b1964cd5b9dca68a099e76f5fc529623a
MisturaO <misturaojulari91@gmail.com>
2023-04-19 09:41:41 -0700

    replaces all attributes of the Student instance

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""A 'Class Student' that retrieves the dictionary representation
+of its instance"""
+
+
+class Student:
+    """Defines a student attributes"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance
+        Arg:
+            attr (dict): If attrs is a list of strings, only attribute names
+            contained in this list must be retrieved.Otherwise, all
+            attributes must be retrieved
+        """
+        dict_rep = self.__dict__.copy()
+        if type(attrs) is list:
+            for value in attrs:
+                if type(value) is not str:
+                    return dict_rep
+            dict_list = {}
+
+            for val in range(len(attrs)):
+                for dict_s in dict_rep:
+                    if attrs[val] == dict_s:
+                        dict_list[dict_s] = dict_rep[dict_s]
+            return dict_list
+        return dict_rep
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student class instance"""
+        for keys, values in json.items():
+            if hasattr(self, keys):
+                setattr(self, keys, values)
