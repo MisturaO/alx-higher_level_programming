cbcf7be59fa358014ead803e4e5c1dee21aa9d0f
MisturaO <misturaojulari91@gmail.com>
2023-04-19 10:02:03 -0700

    returns a list of lists of integers representing the Pascal’s triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A function def pascal_triangle(n): that returns a
+list of lists of integers representing the Pascal’s
+triangle of n
+"""
+
+
+def pascal_triangle(n):
+    res = []
+    if n <= 0:
+        return result
+    for a in range(n):
+        row = []
+        column = 1
+        for b in range(a+1):
+            row.append(column)
+            column = column * (a - b) // (b + 1)
+        res.append(row)
+    return res
