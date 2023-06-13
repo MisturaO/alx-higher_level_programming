44639e5b916848fd93239d8a8c097b4bebacbfb9
MisturaO <misturaojulari91@gmail.com>
2023-06-13 21:59:48 +0100

    "prints x times “C is fun”

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,10 +2,10 @@
 //  prints x times “C is fun”
 const x = parseInt(process.argv[2]);
 
-  if (!x) {
-    console.log('Missing number of occurrences');
-  } else {
-    for (let i = 0; i < x; i++) {
+if (!x) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
 }
