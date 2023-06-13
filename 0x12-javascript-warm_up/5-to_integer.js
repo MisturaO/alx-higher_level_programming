1b1ace512a054da40a490b4763a5888c490fe31d
MisturaO <misturaojulari91@gmail.com>
2023-06-13 21:03:55 +0100

    Converts arg to int

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/*
+    Write a script that prints My number: <first argument 
+    converted in integer> if the first argument can be 
+    converted to an integer 
+*/
+
+let num = parseInt(process.argv[2]);
+
+if ( !num === parseInt) {
+  console.log('Not a number');
+} else {
+  console.log(num);
+}
