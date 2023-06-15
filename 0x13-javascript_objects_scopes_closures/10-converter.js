9affe52d4e2938bd0800647908380b9a1b1ac8c0
MisturaO <misturaojulari91@gmail.com>
2023-06-15 23:48:39 +0100

    convrts number from base 10 to another

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -2,8 +2,8 @@
 //  converts a number from base 10 to another base passed as argument
 
 exports.converter = function (base) {
-    function num (i) {
-        return i.toString(base);
-    }
-    return num;
-}
+  function num (i) {
+    return i.toString(base);
+  }
+  return num;
+};
