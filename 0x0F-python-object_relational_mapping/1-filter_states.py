5be9ed2d80ca6df96371f388706b04d126a8fe70
MisturaO <misturaojulari91@gmail.com>
2023-06-18 19:20:46 +0100

    lists all column in row name starting with 'N'

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A script that lists all states with a name starting with 'N'"""
+
+import MySQLdb
+from sys import argv
+
+"""so my code can only be executed when it's script is run
+directly and not execute when imported into another module"""
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user="bukola",
+                         db="hbtn_0e_0_usa", port=3306, passwd="bukola1091")
+    cur = db.cursor()
+    cur.execute("SELECT name FROM states WHERE name\
+                Like 'N%' ORDER BY name ASC")
+    N_name = cur.fetchall()
+
+    for names in N_name:
+        print(names)
