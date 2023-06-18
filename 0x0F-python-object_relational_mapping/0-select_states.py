f32a3dad7a500cd837d7e6a97f707cab77ae40da
MisturaO <misturaojulari91@gmail.com>
2023-06-18 18:37:33 +0100

    script that lists all states from the database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,10 +3,11 @@
 import MySQLdb
 from sys import argv
 
-
+"""So my code can only be executed when it's script is run 
+directly and not execute when imported into another module"""
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=argv[1],
-                         port=3306, passwd=argv[2], db=argv[3])
+                            port=3306, passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
 
