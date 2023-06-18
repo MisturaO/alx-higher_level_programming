1a62332c9d56b17b5b539dfba82517827b735a60
MisturaO <misturaojulari91@gmail.com>
2023-06-18 19:44:37 +0100

    lists all column in row name starting with 'N'

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
                          port=3306, passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name\
-                Like 'N%' ORDER BY states.id ASC")
+                Like BINARY 'N%' ORDER BY states.id ASC")
     rows = cur.fetchall()
 
     for row in rows:
