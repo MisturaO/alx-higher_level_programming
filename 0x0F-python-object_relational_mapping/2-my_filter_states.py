c5451aef63a8415d2adce5481e3bde97952a26f0
MisturaO <misturaojulari91@gmail.com>
2023-06-19 09:59:32 +0100

    Display where name matches the argument.

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where argument matches the value in
+row 'name' of the table.
+"""
+import MySQLdb
+from sys import argv
+
+
+"""This script runs only if it is run directly as
+a script and not imported into another module"""
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+                         passwd=argv[2], db=argv[3], port=3306)
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        if argv[4] == row[1]:
+            print(row[1])
