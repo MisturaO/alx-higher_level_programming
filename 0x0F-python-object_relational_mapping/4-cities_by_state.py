3ad40023a6a4b1da617358c01f3d7d4fdd55d82d
MisturaO <misturaojulari91@gmail.com>
2023-06-19 14:20:05 +0100

    lists all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -4,15 +4,16 @@ import MySQLdb
 from sys import argv
 
 
-db = MySQLdb.connect(host="localhost", user=argv[1],
-                     passwd=argv[2], db=argv[3], port=3306)
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+                        passwd=argv[2], db=argv[3], port=3306)
 
-cur = db.cursor()
-cur.execute("SELECT cities.id, states.name, cities.name\
-            FROM cities, states\
-            WHERE states.id = cities.state_id\
-            ORDER BY cities.id ASC")
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, states.name, cities.name\
+                FROM cities, states\
+                WHERE states.id = cities.state_id\
+                ORDER BY cities.id ASC")
 
-rows = cur.fetchall()
-for row in rows:
-    print(row)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
