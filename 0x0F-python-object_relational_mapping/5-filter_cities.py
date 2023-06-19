5431c3619255f211fa744a2963a4e5d105bd028d
MisturaO <misturaojulari91@gmail.com>
2023-06-19 15:34:45 +0100

    lists all cities of that state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     cur.execute("SELECT cities.name\
                 FROM cities\
                 JOIN states ON cities.state_id = states.id\
-                WHERE states.name=%s", (argv[4],))
+                WHERE states.name=%s ORDER BY cities.id ASC", (argv[4],))
     rows = cur.fetchall()
 
     # if rows is not None:
