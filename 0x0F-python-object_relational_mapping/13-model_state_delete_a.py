afc043330c4633c583cd4f43b1bd620295e1e9ea
MisturaO <misturaojulari91@gmail.com>
2023-06-20 19:10:52 +0100

    delete all values in the 'name' column in state table

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This script deletes all State objects with a name
+containing the letter 'a' from the database hbtn_0e_6_usa
+"""
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sys import argv
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for instance in session.query(State).filter(State.name.like('%a%')):
+        session.delete(instance)
