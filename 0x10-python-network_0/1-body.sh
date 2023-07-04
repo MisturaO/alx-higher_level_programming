278a06a098254bb4726dacd80b8feca85f8011d4
MisturaO <misturaojulari91@gmail.com>
2023-07-04 14:00:14 +0100

    sends a GET request to the URL

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #This script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-#If there's a redirect -L tells curl to automatically make subsequent requests to the new location until it reaches the final destination
 curl -sL "$1"
