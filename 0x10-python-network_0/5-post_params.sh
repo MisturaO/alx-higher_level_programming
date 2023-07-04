18fd011968976bec9b8c979d9881df55a7387eda
MisturaO <misturaojulari91@gmail.com>
2023-07-04 15:57:45 +0100

    displays the variables posted to the server

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
-#!/bin/b
+#!/bin/bash
 #Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the responses
 curl -sX POST -H "Content_Lenght: 35" -H "Content_Type: mime-type" -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
