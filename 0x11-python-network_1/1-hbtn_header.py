5fe1f2d8e577187394ad55aed8f1a6bc695d1e35
MisturaO <misturaojulari91@gmail.com>
2023-07-05 15:55:16 +0100

    Prints a header value to console

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Displays the value of the 'X-Request-Id' variable found in the header of the response."""
+import urllib.request as request
+from sys import argv
+
+if __name__ == "__main__":
+    url_req = request.Request(argv[1])
+    with request.urlopen(url_req) as response:
+        header_values = response.headers
+        print(header_values['X-Request-Id'])
