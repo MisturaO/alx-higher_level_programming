b02cb83da8ed4677f2941144c92d46e06cb3be51
MisturaO <misturaojulari91@gmail.com>
2023-07-05 20:54:42 +0100

    Make a GET request with request module

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,15 +2,16 @@
 """
 Making a GET request with the 'requests' module:
 This module has methods for GET, POST, DELETE etc and doesn't
-requires some extra step we use in the 'urllib.request' module
-Python script that fetches https://alx-intranet.hbtn.io/status
+requires some extra step we use in the 'urllib.request' module.
+
+This Python script fetches https://alx-intranet.hbtn.io/status
 """
 import requests
 
 if __name__ == "__main__":
     """
     Makes a request with 'get()' and authomatically gets the body
-    response from the server, which i stored in the 'response' var.
+    response from the server, which is stored in the 'response' var.
     'requests' will authomatically decode the body's content.
     """
     response = requests.get('https://alx-intranet.hbtn.io/status')
