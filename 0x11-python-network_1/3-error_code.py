22481d786200115bad933488063c4a8c8b773601
MisturaO <misturaojulari91@gmail.com>
2023-07-05 20:21:51 +0100

    Checks for HTTPError

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,10 +8,16 @@ from sys import argv
 from urllib.error import HTTPError
 
 if __name__ == "__main__":
+    """Gets the request url rom command line arg 1(the script is arg 0)"""
     url_req = request.Request(argv[1])
 
     try:
+        """
+        Opens the url for connection, sends the request and
+        retrieves the response from the server
+        """
         with request.urlopen(url_req) as response:
             print("{}".format(response.read().decode('utf-8')))
     except HTTPError as e:
+        """Checks for HTTPError"""
         print("Error code: {}".format(e.code))
