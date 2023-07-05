fd883a5b134b92227fe6b20935f268a0357f08d6
MisturaO <misturaojulari91@gmail.com>
2023-07-05 16:00:58 +0100

    Prints a header value to console

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Displays the value of the 'X-Request-Id' variable found in the header of the response."""
+"""
+Displays the value of the 'X-Request-Id' variable found
+in the header of the response."""
 import urllib.request as request
 from sys import argv
 
