3cfda98fe31dd7d220a21130f8ba228965e4a627
MisturaO <misturaojulari91@gmail.com>
2023-07-27 15:12:19 +0100

    gets the contents of a webpage and stores it in a file.

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,11 +3,16 @@ const fs = require('fs');
 
 const request = require('request');
 
-// This script gets the contents of a webpage and stores it in a file.
+// This script gets the contents of a webpage and stores it in a file:
+//   first arg: URL to request
+//  second arg: the file path to store the body response
+//  The file is UTF-8 encoded
+
 const UrlReq = process.argv[2];
 const FilePath = process.argv[3];
 
 request(UrlReq, function (_error, _response, body) {
+// if the link passed to the script is not in JSON (e.g HTML) format and you try to deserialize,it will return error
 //   body = JSON.parse(body);
   // console.log(body);
   fs.writeFile(FilePath, body, 'utf-8', function (error) {
