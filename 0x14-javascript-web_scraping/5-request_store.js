10b352232b07501fe6b6f0126de42be2a745d639
MisturaO <misturaojulari91@gmail.com>
2023-07-27 15:06:19 +0100

    gets the contents of a webpage and stores it in a file.

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -10,7 +10,7 @@ const FilePath = process.argv[3];
 request(UrlReq, function (_error, _response, body) {
 //   body = JSON.parse(body);
   // console.log(body);
-    fs.writeFileSync(FilePath, body, 'utf-8', function (error) {
+    fs.writeFile(FilePath, body, 'utf-8', function (error) {
       if (error) {
         console.log(error);
       }
