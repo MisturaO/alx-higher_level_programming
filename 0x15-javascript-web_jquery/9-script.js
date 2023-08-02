9c1ca1f97d101e74dd18cc0b028ae96f2b6d29f4
MisturaO <misturaojulari91@gmail.com>
2023-08-03 00:02:32 +0100

    getch hello

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,11 @@
+// $(document).ready(function () {
+//     $.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
+//     $('DIV#hello').text(data.hello);
+// });
+// });
+
+$('document').ready(function () {
+    $.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
+      $('DIV#hello').text(data.hello);
+    });
+  });
