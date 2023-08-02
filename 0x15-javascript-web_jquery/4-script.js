5d875a85bf51814af1c5261fcd48088c0f6b3577
MisturaO <misturaojulari91@gmail.com>
2023-08-02 22:29:47 +0100

    Toggle classes

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,7 +1,7 @@
 /* global $ */
 
-$('DIV#toggle_header').on("click", function () {
-    const header = $('header')
+$('DIV#toggle_header').on('click', function () {
+  const header = $('header');
 
-    header.toggleClass("red green")
-})
+  header.toggleClass('red green');
+});
