457b742cfe51e523e6d270041c59ac624bcbcb2a
MisturaO <misturaojulari91@gmail.com>
2023-08-03 00:13:58 +0100

    Say Hello!

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,11 +1,6 @@
-// $(document).ready(function () {
-//     $.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
-//     $('DIV#hello').text(data.hello);
-// });
-// });
-
-$('document').ready(function () {
-    $.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
-      $('DIV#hello').text(data.hello);
-    });
+/* global $ */
+$(document).ready(function () {
+  $.get('https://hellosalut.stefanbohacek.dev/?lang=fr', function (data) {
+    $('DIV#hello').text(data.hello);
   });
+});
