ad7c9850157b35258f971715b2c81fa29c024a40
MisturaO <misturaojulari91@gmail.com>
2023-08-03 00:04:04 +0100

    List of elements

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -1,7 +1,7 @@
 /* global $ */
 
-$('DIV#toggle_header').on('click', function () {
-    const newElement = $('<li>Item</li>');
+$('DIV#add_item').on('click', function () {
+  const newElement = $('<li>Item</li>');
 
-    $('UL.my_list').append(newElement)
-})
+  $('UL.my_list').append(newElement);
+});
