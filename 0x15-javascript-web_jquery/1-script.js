864d9d1da20af48b28506625da6ea9e55a7fd06f
MisturaO <misturaojulari91@gmail.com>
2023-08-02 19:04:42 +0100

    Updates the text color of the <header> With JQuery

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,9 @@
+/* global $ */
+// JavaScript script that updates the text color of the <header>
+// element to red (#FF0000) using jQuery API.
+
+// // Stores the element's tag to modify, inside the var 'myHeader'
+const myHeader = $('header');
+
+// Upadtes the <header> tag's text to red
+myHeader.css('color', '#FF0000');
